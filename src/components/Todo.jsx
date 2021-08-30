1f8cf98613523c6a01dbebbb6945a2cd02695bd4
fatihccetin <fatih.ccetin2@gmail.com>
2021-08-30 17:15:04 +0300

    remove tod

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,6 +3,11 @@ import {GrFormClose, GrFormEdit, GrFormCheckmark} from "react-icons/gr";
 import {useTodoLayerValue} from "../context/TodoContext";
 
 const Todo = ({todo}) => {
+    const[{ }, dispatch] = useTodoLayerValue();
+
+    const removeTodo = todoId =>{
+
+    } 
     return (
         <div className="todo-row">
             <div>
@@ -10,7 +15,7 @@ const Todo = ({todo}) => {
             </div>
 
             <div className="todo-icons">
-                      <GrFormClose className="todo-icon" />
+                      <GrFormClose className="todo-icon" onClick={removeTodo(todo.id)} />
                       <GrFormEdit className="todo-icon"/>
 
             </div>            
